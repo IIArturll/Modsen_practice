49c019cad6db0e7d093fd67eeaea11e8acf38cb0
Alexander_Dulski <sasha684549@gmail.com>
2024-06-18 21:31:53 +0300

    refactoring valid in product service

--- backend/product-service/src/main/java/com/example/productservice/core/dto/IngredientDTO.java
+++ backend/product-service/src/main/java/com/example/productservice/core/dto/IngredientDTO.java
@@ -2,6 +2,7 @@ package com.example.productservice.core.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,15 +23,19 @@ public class IngredientDTO {
     private String name;
 
     @NotNull
+    @Positive(message = "calories can't be negative")
     private Integer calories;
 
     @NotNull
+    @Positive(message = "protein can't be negative")
     private Integer protein;
 
     @NotNull
+    @Positive(message = "fats can't be negative")
     private Integer fats;
 
     @NotNull
+    @Positive(message = "carbs can't be negative")
     private Integer carbs;
 
 }
--- backend/product-service/src/main/java/com/example/productservice/core/dto/ProductDTO.java
+++ backend/product-service/src/main/java/com/example/productservice/core/dto/ProductDTO.java
@@ -1,8 +1,6 @@
 package com.example.productservice.core.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,18 +27,23 @@ public class ProductDTO {
     private Integer categoryId;
 
     @NotNull
+    @Positive(message = "calories can't be negative")
     private Integer calories;
 
     @NotNull
+    @Positive(message = "protein can't be negative")
     private Integer protein;
 
     @NotNull
+    @Positive(message = "fats can't be negative")
     private Integer fats;
 
     @NotNull
+    @Positive(message = "carbs can't be negative")
     private Integer carbs;
 
     @NotNull
+    @PositiveOrZero(message = "price can't be negative")
     private Double price;
 
     @NotNull(message = "ingredients must not be null")
